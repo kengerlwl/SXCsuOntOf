448ebdd56f799351d2811fc785ec1fe431718c9c
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-07-08 10:36:41 +0800

    前端 game news 初步設計完成

--- Code/front-end/blog/src/views/news/news.vue
+++ Code/front-end/blog/src/views/news/news.vue
@@ -3,26 +3,35 @@
     <b-navbar type="dark" variant="info">
       <b-navbar-nav>
         <b-nav-item to="school" :active="$route.name === 'school'"
-          >School's New</b-nav-item
+          >School's News</b-nav-item
         >
         <b-nav-item to="game" :active="$route.name === 'game'"
-          >Game's New</b-nav-item
+          >Game's News</b-nav-item
         >
       </b-navbar-nav>
       <b-navbar-nav class="ml-auto">
         <b-nav-form>
-          <b-form-input
-            class="mr-sm-2"
-            placeholder="Search..."
-          ></b-form-input>
+          <b-form-input class="mr-sm-2" placeholder="Search..."></b-form-input>
           <b-button class="my-2 my-sm-0" variant="outline-light"
             >Search</b-button
           >
         </b-nav-form>
       </b-navbar-nav>
     </b-navbar>
-    <b-container>
-      <router-view></router-view>
-    </b-container>
+    <div id="news">
+      <b-container>
+        <router-view></router-view>
+      </b-container>
+    </div>
   </div>
 </template>
+<style>
+#news {
+  background: -webkit-linear-gradient(LightSteelBlue, LightSalmon);
+  background: -o-linear-gradient(LightSteelBlue, LightSalmon);
+  background: -moz-linear-gradient(LightSteelBlue, LightSalmon);
+  background: linear-gradient(LightSteelBlue, LightSalmon);
+  padding-top: 3em;
+  padding-bottom: 3em;
+}
+</style>
