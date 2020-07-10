257af515e3138e0c655794ed540fab9d6a3f6f0d
lgx <1349621303@qq.com>
2020-07-10 15:38:12 +0800

    完成登录注册

--- Code/back-end/java/src/main/java/com/sx/sxblog/service/IUserService.java
+++ Code/back-end/java/src/main/java/com/sx/sxblog/service/IUserService.java
@@ -12,5 +12,17 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2020-07-08
  */
 public interface IUserService extends IService<User> {
+    //通过账号用户名返回个人的全部信息
+    User getUserByUserId(String user_name);
+    //通过用户帐号名和密码获取个人的全部信息
+    User getUserByUserIdAndPassword(String user_name, String password);
+
+    //用户注册的时候对个人信息对填写
+    int insertUserInfo(User user);
+
+    //用户登录后对个人信息进行修改
+    int updateUserInfo(User user);
+
+
 
 }
--- Code/back-end/java/src/main/java/com/sx/sxblog/service/impl/UserServiceImpl.java
+++ Code/back-end/java/src/main/java/com/sx/sxblog/service/impl/UserServiceImpl.java
@@ -1,10 +1,15 @@
 package com.sx.sxblog.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.sx.sxblog.entity.User;
 import com.sx.sxblog.mapper.UserMapper;
 import com.sx.sxblog.service.IUserService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -16,5 +21,32 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+    @Resource
+    private UserMapper userMapper;
 
+    @Override
+    public User getUserByUserId(String user_name) {
+        return userMapper.selectById(user_name);
+    }
+
+    @Override
+    public User getUserByUserIdAndPassword(String user_name, String password) {
+        return userMapper.selectOne(new QueryWrapper<User>()
+                .eq("user_name",user_name)
+                .eq("password",password)
+        );
+    }
+
+    @Override
+    public int insertUserInfo(User user) {
+        return userMapper.insert(user);
+    }
+
+    @Override
+    public int updateUserInfo(User user) {
+        return userMapper.update(user, new QueryWrapper<User>()
+                .eq("user_name",user.getUserName())
+                .eq("password",user.getPassword())
+        );
+    }
 }
