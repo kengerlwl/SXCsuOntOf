6f62b90b1b8d019ac74eabc28737ec816c7cfe1c
lwl <lwl@lwl.localdomain>
2020-07-15 18:05:39 +0800

    完善collect 接口

--- Code/back-end/java/src/main/java/com/sx/sxblog/controller/CollectController.java
+++ Code/back-end/java/src/main/java/com/sx/sxblog/controller/CollectController.java
@@ -3,8 +3,12 @@ package com.sx.sxblog.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.sx.sxblog.common.ReturnEntity;
+import com.sx.sxblog.entity.Blog;
 import com.sx.sxblog.entity.Collect;
+import com.sx.sxblog.entity.Tag;
+import com.sx.sxblog.service.impl.BlogServiceImpl;
 import com.sx.sxblog.service.impl.CollectServiceImpl;
+import com.sx.sxblog.service.impl.TagServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -23,12 +27,57 @@ public class CollectController {
     @Resource
     private CollectServiceImpl collectService;
 
+    @Resource
+    private BlogServiceImpl blogService;
+    @Resource
+    private TagServiceImpl tagService;
     @RequestMapping(value = "/getCollectList",method = RequestMethod.GET)
     @ResponseBody
     public ReturnEntity getCollectList(){
         JSONObject data = new JSONObject();
         List<Collect> collectList = collectService.getCollectList();
         data.put("collectList",collectList);
+        return ReturnEntity.successResult(data);
+    }
+
+    @RequestMapping(value = "/getCollectListByUser",method = RequestMethod.GET)
+    @ResponseBody
+    public ReturnEntity getCollectListByUser(int user_id){
+
+        JSONObject data = new JSONObject();
+        List<Collect> collectList = collectService.getCollectList();
+
+
+        for(int i =0; i < collectList.size(); i++){
+
+
+            Collect collect = collectList.get(i);
+            int UserId = collect.getUserId();
+            System.out.println(UserId);
+            if(UserId != user_id){
+                collectList.remove(i);
+                i--;
+            }else{
+
+                int blog_id = collect.getBlogId();
+
+                String  s = String.valueOf(blog_id);
+
+                Blog blog = blogService.getBlogById(blog_id);
+                List<Tag> tagList = tagService.getTagsByBlogId(blog_id);
+                System.out.println(blog.getBlogContent());
+                data.put(s, blog);
+                data.put(s + "tag", tagList);
+
+            }
+        }
+
+        data.put("collectList",collectList);
+
+
+
+
+
         return ReturnEntity.successResult(data);
     }
 
