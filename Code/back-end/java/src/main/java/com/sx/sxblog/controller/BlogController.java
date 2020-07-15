fc3d8c462280639cd68132f2a9c9c78b823c174a
lwl <lwl@lwl.localdomain>
2020-07-15 14:46:39 +0800

    修改接口

--- Code/back-end/java/src/main/java/com/sx/sxblog/controller/BlogController.java
+++ Code/back-end/java/src/main/java/com/sx/sxblog/controller/BlogController.java
@@ -4,10 +4,13 @@ package com.sx.sxblog.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.sx.sxblog.common.ReturnEntity;
 import com.sx.sxblog.entity.Blog;
+import com.sx.sxblog.entity.Tag;
 import com.sx.sxblog.service.impl.BlogServiceImpl;
+import com.sx.sxblog.service.impl.TagServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -22,6 +25,7 @@ import java.util.List;
 public class BlogController {
     @Resource
     private BlogServiceImpl blogService;
+    @Resource TagServiceImpl tagService;
 
     @RequestMapping(value = "/getBlogList",method = RequestMethod.GET)
     @ResponseBody
@@ -31,6 +35,44 @@ public class BlogController {
         data.put("blogList",blogList);
         return ReturnEntity.successResult(data);
     }
+
+
+
+    @RequestMapping(value = "/getBlogListByUser",method = RequestMethod.GET)
+    @ResponseBody
+    public ReturnEntity getBlogListByUser(int user_id){
+        JSONObject data = new JSONObject();
+
+        List<Blog> blogList = blogService.getBlogList();
+
+
+        for(int    i=0;    i<blogList.size();    i++) {
+            Blog blogtmp    =    blogList.get(i);
+            int userID = blogtmp.getUserId();
+
+            int tmpId =  blogtmp.getBlogId();
+            if (user_id != userID){
+                blogList.remove(i);
+            }
+            else{
+                List<Tag> tagList = tagService.getTagsByBlogId(tmpId);
+                System.out.println(tmpId);
+
+                System.out.println("获取相应的blog tag");
+                JSONObject tmp = new JSONObject();
+                data.put(String.valueOf(tmpId), tagList);
+            }
+
+
+
+        }
+        data.put("bloglist", blogList);
+
+        return ReturnEntity.successResult(data);
+    }
+
+
+
 
     @RequestMapping(value = "/getBlogById",method = RequestMethod.GET)
     @ResponseBody
